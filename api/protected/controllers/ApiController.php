d274990322763e7efc522f0143a8fd924aa54763
Fabiano <fabiano.lothor@gmail.com>
2012-09-01 16:02:01 -0300

    Criado controlador padrão da API

---
+++ api/protected/controllers/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+class ApiController extends Controller
+{
+    public function actionList()
+    {
+		ApplicationHelper::throwException(403);
+    }
+    public function actionView()
+    {
+		ApplicationHelper::throwException(403);
+    }
+    public function actionCreate()
+    {
+		ApplicationHelper::throwException(403);
+    }
+    public function actionUpdate()
+    {
+		ApplicationHelper::throwException(403);
+    }
+    public function actionDelete()
+    {
+		ApplicationHelper::throwException(403);
+    }
+}
