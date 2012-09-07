ef3ccbe6987685f244cceebb6cd9a418d9bebfe6
João Neto <joaoneto20@gmail.com>
2012-09-07 17:09:06 -0300

    ADDED: Tela about do site

--- view/themes/default/views/site/about.php
+++ view/themes/default/views/site/about.php
@@ -1,51 +1,52 @@
 <!-- BEGIN middle part -->
 <section id="content">
-    <div class="href_path_link"><h6>YOU ARE HERE: <a href="icons.html" >FEATURES</a>>TYPOGRAPHY</h6></div>
-    <div class="href_path_about"><h2>TYPOGRAPHY</h2>
+    <!--<div class="href_path_link"><h6>YOU ARE HERE: <a href="icons.html" >FEATURES</a>>TYPOGRAPHY</h6></div> -->
+    <div class="href_path_about"><h2>SOBRE O YOU TROLL</h2>
         <?= CHtml::image(Yii::app()->theme->baseUrl . "/resources/images/end-logo.png") ?>
     </div>
 
     <div class="column column-1-2">
-        <h1 class="no-marg-top">THIS IS H1 HEADER</h1>
+        <h3 class="no-marg-top">HISTÓRICO</h3>
     </div>
 
     <div class="column column-1-2 column-last">
-        <h2 class="no-marg-top padd-top">THIS IS H2 HEADER</h2>
+        <h3 class="no-marg-top padd-top">EQUIPE</h3>
     </div>
 
     <div class="column column-1-2">
-        <p><span class="drop-cap">D</span>olor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Idealizado na aula de Projeto de Negócios do professor Ériko, pelo aluno Fabiano Carneiro, como proposta de estudo para a disciplina. A ideia foi aprovada pelo professor e no período seguinte saiu do papel e está sendo posta em prática como projeto de conclusão do curso de Análise de Sistemas.</p>
     </div>	
 
     <div class="column column-1-2 column-last">
-        <p><span class="drop-cap">L</span>orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.</p>
+        <p><ul>
+            <li>Fabiano Carneiro</li>
+            <li>Ítalo Lelis</li>
+            <li>João Neto</li>
+            <li>Rodolfo Almeida</li>
+        </ul></p>
     </div>
     <div class="clear"></div>
     <div class="con_border marg-top"></div>	
 
     <div class="column column-1-2">
-        <h3 class="padd-top">THIS IS H3 HEADER</h3>
-        <p><span class="drop-cap">C</span>onsectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h3 class="padd-top">LOCALIZAÇÃO</h3>
+        <p>YOU TROLL LTDA;</br>
+            Salas de aula da Faculdade Idez;</br>
+            Casa de Ítalo Lelis;</br>
+            Casa de João Neto;</br>
+            CEP: 58.000-000</br>
+            </p>
     </div>
 
     <div class="column column-1-2 column-last">
-        <h4 class="padd-top">THIS IS H4 HEADER</h4>
-        <p><span class="drop-cap">S</span>it amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h3 class="padd-top">E-COMMERCE</h3>
+        <p>Os produtos com a marca do youtrol serão encontrados no nosso e-commerce, dispomos de diversos produtos como camisetas, canecas, sandálias etc. Alguns produtos são personalizáveis, e da mais alta qualidade.</p>
     </div>
 
     <div class="clear"></div>
     <div class="con_border marg-top"></div>	
 
 
-    <div class="column column-1-2">	
-        <h5 class="padd-top">THIS IS H5 HEADER</h5>
-        <p><span class="drop-cap">A</span>met, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-
-    <div class="column column-1-2 column-last">
-        <h6 class="padd-top">THIS IS H6 HEADER</h6>
-        <p><span class="drop-cap">C</span>onsectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
 
     <div class="clear"></div>
 
