a18b94a706208fed667a917b566d16ed8933457d
João Neto <joaoneto20@gmail.com>
2012-09-08 16:21:59 -0300

    ADDED: Arquivos de internacionalização

---
+++ view/protected/messages/pt_br/texts.php
@@ -0,0 +1,27 @@
+<?php
+
+return array(
+    // Páginas Abertas - Visíveis apenas para os visitantes
+    'sobre' => 'SOBRE O YOU TROLL',
+    'historico' => 'HISTÓRICO',
+    'equipe' => 'EQUIPE',
+    'localizacao' => 'LOCALIZAÇÃO',
+    'terms' => 'TERMOS',
+    'aceotacao' => 'Aceitação',
+    
+    
+    // Textos páginas abertas
+    
+    'textHistorico' => 'Idealizado na aula de Projeto de Negócios do professor Ériko, pelo aluno Fabiano Carneiro, como proposta de estudo para a disciplina. A ideia foi aprovada pelo professor e no período seguinte saiu do papel e está sendo posta em prática como projeto de conclusão do curso de Análise de Sistemas.',
+    
+    'textEcommerce' => 'Os produtos com a marca do youtrol serão encontrados no nosso e-commerce, dispomos de diversos produtos como camisetas, canecas, sandálias etc. Alguns produtos são personalizáveis, e da mais alta qualidade.',
+    
+    'textAceitacao' => '
+Ao usar ou visitar o site You Troll ou qualquer produtos do You Troll, programas, fornecimento de dados e serviços fornecidos para você, a partir ou através do You Troll (coletivamente, "Serviços"), você manifesta a sua concordância com estes termos e condições ("Termos de Serviço"), o aviso de privacidade do You Troll, localizado no site http://www.youtroll.com.br/privacy e incorporado ao presente como referência, e as Diretrizes da Comunidade do You Troll, localizadas no site http://www.youtroll.com.br/directress e também incorporadas ao presente como referência. Se Você não concordar com algum destes termos, com o aviso de privacidade do You Troll ou com as Diretrizes da Comunidade, não deverá utilizar o Serviço.</br></br>
+Embora nós nos esforcemos para avisar Você sempre que mudanças importantes forem realizadas nestes Termos de Serviço, Você deve reler periodicamente a versão mais atualizada, (www.youtroll.com.br/terms). O You Troll pode, a seu exclusivo critério, modificar ou revisar estes Termos de Serviço e suas políticas a qualquer tempo, e Você concorda em cumprir tais modificações ou revisões. Nada nestes Termos de Serviço será tido como concessão de quaisquer direitos ou benefícios a terceiros.</br></br>
+É importante saber que o Website é controlado e oferecido pelo You Troll a partir de suas instalações no Brasil. O You Troll não garante que o website do You Troll seja apropriado ou esteja disponível para uso em outros locais. As pessoas que acessam ou usam o website do You Troll a partir de outras jurisdições o fazem por conta própria e são responsáveis pelo cumprimento das leis regionais/nacionais.</br></br>
+Quanto à capacidade para aceitar os Termos de Uso, Você afirma ser maior de 18 anos ou ser menor emancipado, ou estar de posse de autorização legal dos pais ou de tutores, e plenamente capaz de consentir com os termos, condições, obrigações, afirmações, representações e garantias descritas nestes Termos de Uso, e obedecê-los e cumpri-los. Em qualquer circunstância, Você afirma ter mais de 18 anos, visto que o website do You Troll não é projetado para jovens menores de 18 anos. Se Você tiver menos de 18 anos, não deverá utilizar o website do You Troll. Você deverá conversar com seus pais sobre quais sites são apropriados para ele.' 
+    
+);
+
+?>
