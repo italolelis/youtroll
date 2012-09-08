c55a49703efc6b75c1f99fd96e4c207d52eeb5ce
João Neto <joaoneto20@gmail.com>
2012-09-08 16:22:42 -0300

    UPDATE: Internacionalização tela about

--- view/themes/default/views/site/about.php
+++ view/themes/default/views/site/about.php
@@ -1,46 +1,48 @@
 <!-- BEGIN middle part -->
 <section id="content">
     <!--<div class="href_path_link"><h6>YOU ARE HERE: <a href="icons.html" >FEATURES</a>>TYPOGRAPHY</h6></div> -->
-    <div class="href_path_about"><h2><?=Yii::t('app', 'sobre')?></h2>
+    <div class="href_path_about"><h2><?=Yii::t('texts', 'sobre')?></h2>
         <?= CHtml::image(Yii::app()->theme->baseUrl . "/resources/images/end-logo.png") ?>
     </div>
 
     <div class="column column-1-2">
-        <h3 class="no-marg-top">HISTÓRICO</h3>
+        <h3 class="no-marg-top"><?=Yii::t('texts', 'historico')?></h3>
     </div>
 
     <div class="column column-1-2 column-last">
-        <h3 class="no-marg-top padd-top">EQUIPE</h3>
+        <h3 class="no-marg-top padd-top"><?=Yii::t('texts', 'equipe')?></h3>
     </div>
 
     <div class="column column-1-2">
-        <p>Idealizado na aula de Projeto de Negócios do professor Ériko, pelo aluno Fabiano Carneiro, como proposta de estudo para a disciplina. A ideia foi aprovada pelo professor e no período seguinte saiu do papel e está sendo posta em prática como projeto de conclusão do curso de Análise de Sistemas.</p>
+        <p><?=Yii::t('texts', 'textHistorico')?></p>
     </div>	
 
     <div class="column column-1-2 column-last">
-        <p><ul>
+        <p>
+        <ul>
             <li>Fabiano Carneiro</li>
             <li>Ítalo Lelis</li>
             <li>João Neto</li>
             <li>Rodolfo Almeida</li>
-        </ul></p>
+        </ul>
+        </p>
     </div>
     <div class="clear"></div>
     <div class="con_border marg-top"></div>	
 
     <div class="column column-1-2">
-        <h3 class="padd-top">LOCALIZAÇÃO</h3>
+        <h3 class="padd-top"><?=Yii::t('texts', 'localizacao')?></h3>
         <p>YOU TROLL LTDA;</br>
             Salas de aula da Faculdade Idez;</br>
             Casa de Ítalo Lelis;</br>
             Casa de João Neto;</br>
             CEP: 58.000-000</br>
-            </p>
+        </p>
     </div>
 
     <div class="column column-1-2 column-last">
         <h3 class="padd-top">E-COMMERCE</h3>
-        <p>Os produtos com a marca do youtrol serão encontrados no nosso e-commerce, dispomos de diversos produtos como camisetas, canecas, sandálias etc. Alguns produtos são personalizáveis, e da mais alta qualidade.</p>
+        <p><?=Yii::t('texts', 'textEcommerce')?></p>
     </div>
 
     <div class="clear"></div>
