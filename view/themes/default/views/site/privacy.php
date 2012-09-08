f3fcd067efc50dabf6ef2af89706984f4a95322a
João Neto <joaoneto20@gmail.com>
2012-09-08 16:54:06 -0300

    ADDED: Tela política de privacidade

--- view/themes/default/views/site/privacy.php
+++ view/themes/default/views/site/privacy.php
@@ -1,51 +1,31 @@
 <!-- BEGIN middle part -->
 <section id="content">
-    <div class="href_path_link"><h6>YOU ARE HERE: <a href="icons.html" >FEATURES</a>>TYPOGRAPHY</h6></div>
-    <div class="href_path_about"><h2>TYPOGRAPHY</h2>
+    <div class="href_path_about"><h2><?=__('privacidade', 'texts')?></h2>
         <?= CHtml::image(Yii::app()->theme->baseUrl . "/resources/images/end-logo.png") ?>
     </div>
 
+    <div class="column">
+        <p><?=__('textPrivacidade', 'texts')?></p>
+    </div>
+    
     <div class="column column-1-2">
-        <h1 class="no-marg-top">THIS IS H1 HEADER</h1>
+        <h3 class="no-marg-top"><?=__('informacoesQueRecolhemos', 'texts')?></h3>
     </div>
-
-    <div class="column column-1-2 column-last">
-        <h2 class="no-marg-top padd-top">THIS IS H2 HEADER</h2>
+    
+    <div class="column">
+        <p><?=__('textInformacoeQueRecolhemos', 'texts')?></p>
     </div>
-
+    
     <div class="column column-1-2">
-        <p><span class="drop-cap">D</span>olor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>	
-
-    <div class="column column-1-2 column-last">
-        <p><span class="drop-cap">L</span>orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.</p>
+        <h3 class="no-marg-top"><?=__('comoUtilizamos', 'texts')?></h3>
     </div>
-    <div class="clear"></div>
-    <div class="con_border marg-top"></div>	
-
-    <div class="column column-1-2">
-        <h3 class="padd-top">THIS IS H3 HEADER</h3>
-        <p><span class="drop-cap">C</span>onsectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-
-    <div class="column column-1-2 column-last">
-        <h4 class="padd-top">THIS IS H4 HEADER</h4>
-        <p><span class="drop-cap">S</span>it amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    
+    <div class="column">
+        <p><?=__('textComoUtilizamos', 'texts')?></p>
     </div>
 
     <div class="clear"></div>
     <div class="con_border marg-top"></div>	
-
-
-    <div class="column column-1-2">	
-        <h5 class="padd-top">THIS IS H5 HEADER</h5>
-        <p><span class="drop-cap">A</span>met, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-
-    <div class="column column-1-2 column-last">
-        <h6 class="padd-top">THIS IS H6 HEADER</h6>
-        <p><span class="drop-cap">C</span>onsectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
 
     <div class="clear"></div>
 
