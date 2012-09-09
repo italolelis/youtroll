4def30deb41e26e178665af7914c565faf6f155c
italolelis <italolelis@gmail.com>
2012-09-09 03:01:14 -0300

    Criado view de listagem de categorias

--- view/protected/modules/admin/views/category/index.php
+++ view/protected/modules/admin/views/category/index.php
@@ -9,44 +9,41 @@
                     <?= __("Nome", "admin"); ?>
                 </th>
                 <th>
-                    <?= __("E-mail", "admin"); ?>
-                </th>
-                <th>
                     <?= __("Ações", "admin"); ?>
                 </th>
             </tr>
         </thead>
         <tbody>    
-            <?php foreach ($model as $m): ?>
-                <tr>
-                    <td>
-                        <?= $m->usr_id; ?>
-                    </td>
-                    <td>
-                        <?= $m->usr_username; ?>
-                    </td>
-                    <td>
-                        <?= $m->usr_email; ?>
-                    </td>
-                    <td class="button-column">
-                        <?php
-                        $this->widget('bootstrap.widgets.TbButton', array(
-                            'label' => 'Edit',
-                            'size' => 'mini',
-                            'type' => 'primary',
-                            'url' => Yii::app()->createAbsoluteUrl("admin/user/" . $m->usr_id),
-                            'htmlOptions' => array(
-                                'data-toggle' => 'modal',
-                                'class' => 'edit',
-                                'data-target' => '#editModal',
-                            ),
-                        ));
-                        ?>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
+            <?php if (!empty($model)): ?>
+                <?php foreach ($model as $m): ?>
+                    <tr>
+                        <td>
+                            <?= $m->cmc_ctg_id; ?>
+                        </td>
+                        <td>
+                            <?= $m->cmc_ctg_description; ?>
+                        </td>
+                        <td class="button-column">
+                            <?php
+                            $this->widget('bootstrap.widgets.TbButton', array(
+                                'label' => 'Edit',
+                                'size' => 'mini',
+                                'type' => 'primary',
+                                'url' => Yii::app()->createAbsoluteUrl("admin/category/" . $m->cmc_ctg_id),
+                                'htmlOptions' => array(
+                                    'data-toggle' => 'modal',
+                                    'class' => 'edit',
+                                    'data-target' => '#editModal',
+                                ),
+                            ));
+                            ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </tbody>
     </table>
+    <a class="toggle-link" href="<?= Yii::app()->createAbsoluteUrl("admin/category/create") ?>"><i class="icon-plus"></i> <?= __("New Category", "admin") ?></a>
 </div>
 
 <?= $this->renderPartial('application.modules.admin.views.user.modals.editModal') ?>
