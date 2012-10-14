e00e9d59164b6e90cd561018de447941868e0c19
FabianoLothor <fabiano.lothor@gmail.com>
2012-10-14 03:02:41 -0300

    Dado início a criação da página do painel de controle do site.

--- project/themes/default/views/user/controlPanel.php
+++ project/themes/default/views/user/controlPanel.php
@@ -1,1 +1,46 @@
 <h1 class="page-title"><?= HApp::t('controlPanelTitle') ?></h1>
+
+<div>
+    <ul class="tabs-nav">
+        <li class="active">
+            <a href="#controlPanelTab1"><?= HApp::t('myImages') ?></a>
+        </li>
+        <li>
+            <a href="#controlPanelTab2"><?= HApp::t('mySessions') ?></a>
+        </li>
+        <li>
+            <a href="#controlPanelTab3"><?= HApp::t('comments') ?></a>
+        </li>
+        <li>
+            <a href="#controlPanelTab4"><?= HApp::t('inscriptions') ?></a>
+        </li>
+        <li>
+            <a href="#controlPanelTab5"><?= HApp::t('accountStats') ?></a>
+        </li>
+        <li>
+            <a href="#controlPanelTab6"><?= HApp::t('settings') ?></a>
+        </li>
+    </ul>
+
+    <div class="tabs-container">
+        <div class="tab-content" id="controlPanelTab1">
+            1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condim entum ultrices consequat eu, vehicula ac mauris. Ut adipiscing, leo nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+        <div class="tab-content displayNone" id="controlPanelTab2">
+            2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condim entum ultrices consequat eu, vehicula ac mauris. Ut adipiscing, leo nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+        <div class="tab-content displayNone" id="controlPanelTab3">
+            3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condim entum ultrices consequat eu, vehicula ac mauris. Ut adipiscing, leo nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+        <div class="tab-content displayNone" id="controlPanelTab4">
+            3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condim entum ultrices consequat eu, vehicula ac mauris. Ut adipiscing, leo nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+        <div class="tab-content displayNone" id="controlPanelTab5">
+            3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condim entum ultrices consequat eu, vehicula ac mauris. Ut adipiscing, leo nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+        <div class="tab-content displayNone" id="controlPanelTab6">
+            3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisl orci, condim entum ultrices consequat eu, vehicula ac mauris. Ut adipiscing, leo nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+    </div>
+
+</div>
