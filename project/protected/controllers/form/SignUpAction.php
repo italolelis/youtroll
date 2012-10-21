35b792a86bb821c034995ee9491c52ece983dfd5
FabianoLothor <fabiano.lothor@gmail.com>
2012-10-21 12:01:45 -0300

    Melhoria de visibilidade no código.

--- project/protected/controllers/form/SignUpAction.php
+++ project/protected/controllers/form/SignUpAction.php
@@ -24,6 +24,7 @@ class SignUpAction extends CAction
                     }
                     
                     $model->addErrors($response);
+                    
                     HModel::generatePerformAjaxErrors($model);
                 }
                 
