be09aa5a06b86d9dcb3d04f8352dcf91a3a7d3fb
FabianoLothor <fabiano.lothor@gmail.com>
2012-10-21 03:46:21 -0300

    Criação do Controlador de Publicações na API.

---
+++ rest/protected/controllers/PublicationController.php
@@ -0,0 +1,33 @@
+<?php
+
+Yii::import('application.components.Controller');
+
+class PublicationController extends Controller {
+
+    public function init() {
+        parent::init();
+
+        $this->model = new Publication();
+    }
+
+    public function actionInsert() {
+        if (Yii::app()->request->isPostRequest) {
+            $user = User::model()->findByPk($this->headers->Authorization[0]);
+            foreach ($user->channel as $key => $value) {
+                var_dump($value);
+            }exit();
+            $this->model->setAttributesWithoutPrefix($_POST);
+            foreach ($user->publications as $key => $value) {
+                var_dump($value);
+            }exit();
+            $this->model->setAttributeWithoutPrefix($user->getAttributeWithoutPrefix('id'), 'owner');
+            $this->model->setAttributeWithoutPrefix($user->channel->getAttributeWithoutPrefix('id'), 'channel');
+            
+            if (!$this->model->save()) {
+                HApp::ajaxResponse($this->model->getErrors(), $this->model->getAttributesPrefix());
+            }
+            
+            HApp::ajaxResponse('true');
+        }
+    }
+}
