35b0be7a7514ae58f894bcbabc0aaee1fea7adbe
FabianoLothor <fabiano.lothor@gmail.com>
2012-10-25 15:48:14 -0300

    Adicionados botões de like e unlike na página de publicação.

--- project/themes/default/views/site/publication.php
+++ project/themes/default/views/site/publication.php
@@ -1,16 +1,18 @@
 <h2 class="page-title"><?= $publication->title ?></h2>
 <div id="publication" class="two-third">
     <div id="imageHeader">
-        <?= CHtml::button($owner->email, array('class' => 'button large usernameStyle')) ?>
+        <?= CHtml::button($owner->email, array('class' => 'button large channelStyle')) ?>
         <?= CHtml::button(HApp::t('subscribe'), array('class' => 'button large')) ?>
     </div>
     <div id="image" class="marginTop">
         <?= CHtml::image(Yii::app()->baseUrl . "/resources/img/user/$owner->email/$publication->image_path", $publication->title) ?>
     </div>
     <div id="imageButtons">
-        
+        <?= CHtml::button(HApp::t('like'), array('class' => 'button medium buttonStyle')) ?>
+        <?= CHtml::button('', array('class' => 'button medium buttonStyle')) ?>
+        <?= CHtml::button(HApp::t('share'), array('class' => 'button medium buttonStyle')) ?>
     </div>
-    <div id="imageDate">
+    <div id="imageDate" class="marginTop">
         <?= Yii::t('app', $owner->name ? 'publicationDateWithName' : 'publicationDate', array('{date}' => $publication->record, '{name}' => $owner->name)); ?>
     </div>
     <div id="imageDescription" class="infobox">
