178aaaed57e432b7303bee208880f158dab7d297
FabianoLothor <fabiano.lothor@gmail.com>
2012-11-05 21:08:05 -0300

    Importação de alguns scripts JS ao projeto.

--- project/themes/default/views/layouts/main/head/scripts.php
+++ project/themes/default/views/layouts/main/head/scripts.php
@@ -2,14 +2,14 @@
 <?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/themeCustom.js') ?>
 <?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/themeJCycle.js') ?>
 <?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/themeJFancybox.js') ?>
+<?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/themeJEasing.js') ?>
+<?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/themeJCarousel.min.js') ?>
 
 <!--
 <?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/themeJGMap.js') ?>
-<?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/themeJIsotope.js') ?>
-<?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/themeJCarousel.js') ?>
-<?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/themeJSmartStartSlider.js') ?>
-<?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/themeJEasing.js') ?>
+<?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/themeJIsotope.min.js') ?>
 <?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/themeRespond.js') ?>
+<?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/themeJSmartStartSlider.js') ?>
 -->
 
 <?= CHtml::scriptFile(Yii::app()->baseUrl . '/resources/js/general.js') ?>
