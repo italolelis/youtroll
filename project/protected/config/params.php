b59369d9ba950d52ddd09fe643c08aa922ec304b
FabianoLothor <fabiano.lothor@gmail.com>
2012-11-06 01:34:36 -0300

    Adicionado parametrização do total de visualizações que deverão ser exibidas.

--- project/protected/config/params.php
+++ project/protected/config/params.php
@@ -6,6 +6,7 @@ return array(
     
     'allowedExtensions' => array('jpg', 'gif', 'png'),
     'maxSizeUpload' => 1048576, // 1MB
+    'maxPublications' => 32,
     
     'defaultLanguage' => 'pt_br',
     'supportedLanguages' => array(
