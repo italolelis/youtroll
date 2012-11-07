ac1cb179e4d0537200cd4cc2ef20f18e0431ed6b
FabianoLothor <fabiano.lothor@gmail.com>
2012-11-07 13:19:37 -0300

    Organização de código.

--- project/themes/default/views/site/index.php
+++ project/themes/default/views/site/index.php
@@ -5,13 +5,18 @@
 <?php
 $this->widget('ext.JCarousel.JCarousel', array(
     'dataProvider' => new CArrayDataProvider($recentPublications),
+    
     'thumbUrl' => 'str_replace(".", "_thumb.", Yii::app()->baseUrl . "/resources/img/user/{$data->owner}/{$data->image_path}")',
     'imageUrl' => 'Yii::app()->baseUrl . "/resources/img/user/{$data->owner}/{$data->image_path}"',
-    'altText' => '$data->title',
-    'titleText' => '$data->title',
-    'categories' => 'Category::getNameByID($data->category)',
-    'skin' => 'yt',
-    'clickCallback' => '',
     'target' => '#',
+    
+    'titleText' => '$data->title',
+    'altText' => '$data->title',
+    'categories' => 'Category::getNameByID($data->category)',
+    
+    'cssFile' => false,
+    'skin' => 'yt',
+    
+    'clickCallback' => '',
 ));
 ?>
