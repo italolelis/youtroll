c4b4700cdbe41fd00371245c7dbf7e64417ef555
FabianoLothor <fabiano.lothor@gmail.com>
2012-11-07 12:49:40 -0300

    Finalização da exibição das Publicações mais recentes na Página Inicial.

--- project/themes/default/views/site/index.php
+++ project/themes/default/views/site/index.php
@@ -2,24 +2,16 @@
 
 <h6 class="section-title"><?= HApp::t('recentPublications') ?></h6>
 
-<div class="jcarousel-container jcarousel-container-horizontal jcarousel-container-div">
-    <div class="jcarousel-clip jcarousel-clip-horizontal jcarousel-clip-div">
-        <ul class="projects-carousel clearfix jcarousel-list jcarousel-list-horizontal jcarousel-list-ul">
-            <?php $totalPublications = 0; ?>
-
-            <?php foreach($recentPublications as $publication): ?>
-                <?php ++$totalPublications ?>
-
-                <li class="jcarousel-item jcarousel-item-horizontal jcarousel-item-<?= $totalPublications ?> jcarousel-item-<?= $totalPublications ?>-horizontal jcarousel-item-li" jcarouselindex="<?= $totalPublications ?>">
-                    <a href="single-project.html">
-                        <?= CHtml::image(str_replace('.', '_thumb.', Yii::app()->baseUrl . "/resources/img/user/$publication->owner/$publication->image_path"), $publication->title) ?>
-                        <h5 class="title"><?= $publication->title ?></h5>
-                        <span class="categories"><?= Category::getNameByID($publication->category) ?></span>
-                    </a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
-    <div class="jcarousel-prev jcarousel-prev-horizontal jcarousel-prev-disabled jcarousel-prev-disabled-horizontal" style="display: block;" disabled="disabled"></div>
-    <div class="jcarousel-next jcarousel-next-horizontal" style="display: block;"></div>
-</div>
+<?php
+$this->widget('ext.JCarousel.JCarousel', array(
+    'dataProvider' => new CArrayDataProvider($recentPublications),
+    'thumbUrl' => 'str_replace(".", "_thumb.", Yii::app()->baseUrl . "/resources/img/user/{$data->owner}/{$data->image_path}")',
+    'imageUrl' => 'Yii::app()->baseUrl . "/resources/img/user/{$data->owner}/{$data->image_path}"',
+    'altText' => '$data->title',
+    'titleText' => '$data->title',
+    'categories' => 'Category::getNameByID($data->category)',
+    'skin' => 'yt',
+    'clickCallback' => '',
+    'target' => '#',
+));
+?>
