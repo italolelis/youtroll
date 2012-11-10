7ea8c7ca530e79929cdb2315ebda623e549a64a3
italolelis <italolelis@gmail.com>
2012-11-10 02:10:21 -0300

    Adicionado links no footer

--- project/themes/default/views/layouts/main/footer.php
+++ project/themes/default/views/layouts/main/footer.php
@@ -1,3 +1,37 @@
 <div class="container">
-    
+    <div class="three-fourth">
+
+        <nav id="footer-nav" class="clearfix">
+
+            <ul>
+                <li><a href="<?= Yii::app()->baseUrl ?>">Home</a></li>
+                <li><a href="<?= Yii::app()->baseUrl . "/about" ?>">Sobre</a></li>
+                <li><a href="<?= Yii::app()->baseUrl . "/terms" ?>">Termos de privacidade</a></li>
+                <li><a href="<?= Yii::app()->baseUrl . "/contact" ?>">Contato</a></li>
+            </ul>
+
+        </nav><!-- end #footer-nav -->
+
+        <ul class="contact-info">
+            <li class="address">012 Some Street. New York, NY, 12345. USA</li>
+            <li class="phone">(123) 456-7890</li>
+            <li class="email"><a href="mailto:contato@youtroll.com.br">contato@youtroll.com.br</a></li>
+        </ul><!-- end .contact-info -->
+
+    </div><!-- end .three-fourth -->
+
+    <div class="one-fourth last">
+
+        <span class="title"><?= HApp::t('Esteja em contato'); ?></span>
+
+        <ul class="social-links">
+            <li class="twitter"><a href="#">Twitter</a></li>
+            <li class="facebook"><a href="#">Facebook</a></li>
+            <li class="digg"><a href="#">Digg</a></li>
+            <li class="vimeo"><a href="#">Vimeo</a></li>
+            <li class="youtube"><a href="#">YouTube</a></li>
+            <li class="skype"><a href="#">Skype</a></li>
+        </ul><!-- end .social-links -->
+
+    </div><!-- end .one-fourth.last -->
 </div>
