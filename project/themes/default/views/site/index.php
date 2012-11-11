de43fc94050548ca6b2f0ff628e76ba7db046aca
FabianoLothor <fabiano.lothor@gmail.com>
2012-11-11 03:04:05 -0300

    Otimização da Visualização Inicial, utilizando o Utilitário jCarousel para exibir as listagens das publicações.

--- project/themes/default/views/site/index.php
+++ project/themes/default/views/site/index.php
@@ -1,21 +1,6 @@
 <h2 class="page-title align-center"><?= HApp::t('slogan') ?></h2>
 
-<?php
-$this->widget('ext.JCarousel.JCarousel', array(
-    'dataProvider' => new CArrayDataProvider($recentPublications),
-    
-    'thumbUrl' => 'str_replace(".", "_thumb.", Yii::app()->baseUrl . "/resources/img/user/{$data->owner}/{$data->image_path}")',
-    'imageUrl' => 'Yii::app()->baseUrl . "/resources/img/user/{$data->owner}/{$data->image_path}"',
-    'target' => '#',
-    
-    'summaryText' => '<h6 class="section-title">'.HApp::t("recentPublications").'</h6>',
-    'titleText' => '$data->title',
-    'altText' => '$data->title',
-    'categories' => 'Category::getNameByID($data->category)',
-    
-    'cssFile' => false,
-    'skin' => 'yt',
-    
-    'clickCallback' => '',
-));
-?>
+<?= $this->renderPartial('utils/jCarousel', array('title' => 'recentPublications', 'publications' => $recentPublications)) ?>
+<?= $this->renderPartial('utils/jCarousel', array('title' => 'popularPublications', 'publications' => $popularPublications)) ?>
+<?= $this->renderPartial('utils/jCarousel', array('title' => 'mostViewedPublications', 'publications' => $mostViewedPublications)) ?>
+<?= $this->renderPartial('utils/jCarousel', array('title' => 'lessViewedPublications', 'publications' => $lessViewedPublications)) ?>
