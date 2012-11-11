8ec7000e7e259a34ab874535e9dfdf7fd6e0f113
FabianoLothor <fabiano.lothor@gmail.com>
2012-11-11 04:12:49 -0300

    Organização do cabeçalho do site.

--- project/themes/default/views/layouts/main/head.php
+++ project/themes/default/views/layouts/main/head.php
@@ -7,8 +7,8 @@
 <?= CHtml::metaTag('index, follow', 'robots') ?>
 <?= CHtml::metaTag('index, follow', 'googlebot') ?>
 
-<?php Yii::app()->clientScript->registerLinkTag('shortcut icon', 'image/png', Yii::app()->baseUrl . '/resources/img/favicon.png'); ?>
-
 <title><?= HApp::t('name') ?></title>
 
 <?= $this->renderPartial('webroot.themes.' . Yii::app()->theme->name . '.views.layouts.' . Yii::app()->layout . '.head.styles') ?>
+
+<?php Yii::app()->clientScript->registerLinkTag('shortcut icon', 'image/png', Yii::app()->baseUrl . '/resources/img/favicon.png'); ?>
