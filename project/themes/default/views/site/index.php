4b0a6db506862d9cd0a8b8f00a2f8fd429645658
FabianoLothor <fabiano.lothor@gmail.com>
2012-11-11 03:13:29 -0300

    Utilitário jCarousel renomeado para publicationsList

--- project/themes/default/views/site/index.php
+++ project/themes/default/views/site/index.php
@@ -1,6 +1,6 @@
 <h2 class="page-title align-center"><?= HApp::t('slogan') ?></h2>
 
-<?= $this->renderPartial('utils/jCarousel', array('title' => 'recentPublications', 'publications' => $recentPublications)) ?>
-<?= $this->renderPartial('utils/jCarousel', array('title' => 'popularPublications', 'publications' => $popularPublications)) ?>
-<?= $this->renderPartial('utils/jCarousel', array('title' => 'mostViewedPublications', 'publications' => $mostViewedPublications)) ?>
-<?= $this->renderPartial('utils/jCarousel', array('title' => 'lessViewedPublications', 'publications' => $lessViewedPublications)) ?>
+<?= $this->renderPartial('utils/publicationsList', array('title' => 'recentPublications', 'publications' => $recentPublications)) ?>
+<?= $this->renderPartial('utils/publicationsList', array('title' => 'popularPublications', 'publications' => $popularPublications)) ?>
+<?= $this->renderPartial('utils/publicationsList', array('title' => 'mostViewedPublications', 'publications' => $mostViewedPublications)) ?>
+<?= $this->renderPartial('utils/publicationsList', array('title' => 'lessViewedPublications', 'publications' => $lessViewedPublications)) ?>
