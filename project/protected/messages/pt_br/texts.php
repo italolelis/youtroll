2576264fb5cda858c1daa35b46c7d47dbb179d55
FabianoLothor <fabiano.lothor@gmail.com>
2012-11-13 03:14:57 -0300

    Criação da página Sobre do site.

--- project/protected/messages/pt_br/texts.php
+++ project/protected/messages/pt_br/texts.php
@@ -1,5 +1,25 @@
 <?php
 
 return array(
-
+    'about_1_1.1' => 'O <strong>YouTroll</strong>, é uma rede de compartilhamento de conteúdo cômico.',
+    'about_1_1.2' => 'A proposta é oferecer um local onde os usuários possam visualizar imagens de diversos canais, em um só local, além de possuirem liberdade de publicar suas próprias criações de forma fácil e rápida.',
+    
+    'about_1_2' => 'Acompanhe seus canais favoritos',
+    'about_1_2.1' => 'Agora ficou fácil ver aquela imagem engraçada que o seu canal favorito acabou de publicar.',
+    'about_1_2.2' => 'Inscrevendo-se nos canais de outros usuários, você será imediatamente notificado quando uma nova publicação for feita.',
+    
+    'about_1_3' => 'Compartilhe com seus amigos',
+    'about_1_3.1' => 'No <strong>YouTroll</strong> podes compartilhar as imagens com todos os seus amigos, através das principais redes sociais de forma rápida e objetiva.',
+    
+    'about_1_4' => 'Produza seu próprio conteúdo',
+    'about_1_4.1' => 'Você pode produzir e divulgar para seus fãs e amigos, imagens que satirizam o seu dia-a-dia, ou mesmo, cenas cotidianas. As publicações podem ser tanto enviadas, quanto criadas no próprio site.',
+    
+    'about_1_5' => 'Contato',
+    'about_1_5.1' => 'Sinta-se à vontade para entrar em contato conosco em caso de dúvidas sobre nossos serviços, propostas ou parcerias, estaremos sempre à disposição.',
+    
+    'about_2_1' => 'Equipe',
+    
+    'about_2_2' => 'Participe',
+    'about_2_2.1' => 'Deseja colaborar com o YouTroll?',
+    'about_2_2.2' => 'Envie-nos seu CV e faça parte de nossa equipe.',
 );
--- project/themes/default/views/ajax/about.php
+++ project/themes/default/views/ajax/about.php
@@ -1,1 +1,40 @@
 <h1 class="page-title"><?= HApp::t('aboutTitle') ?></h1>
+
+<div class="two-third alignJustify">
+    <div class="infobox">
+        <div><?= HApp::t('about_1_1.1', 'texts') ?></div>
+        <div class="marginTop"><?= HApp::t('about_1_1.2', 'texts') ?></div>
+        
+        <h5 class="section-title marginTop"><?= HApp::t('about_1_2', 'texts') ?></h5>
+        <div class="marginTop"><?= HApp::t('about_1_2.1', 'texts') ?></div>
+        <div class="marginTop"><?= HApp::t('about_1_2.2', 'texts') ?></div>
+        
+        <h5 class="section-title marginTop"><?= HApp::t('about_1_3', 'texts') ?></h5>
+        <div class="marginTop"><?= HApp::t('about_1_3.1', 'texts') ?></div>
+        
+        <h5 class="section-title marginTop"><?= HApp::t('about_1_4', 'texts') ?></h5>
+        <div class="marginTop"><?= HApp::t('about_1_4.1', 'texts') ?></div>
+        
+        <h5 class="section-title marginTop"><?= HApp::t('about_1_5', 'texts') ?></h5>
+        <div class="marginTop"><?= HApp::t('about_1_5.1', 'texts') ?></div>
+        <ul class="arrow-2 dotted marginTop">
+            <li><strong><?= HApp::t('email') ?>:</strong> <?= CHtml::link(HApp::t('contactEmail'), 'mailto:'.HApp::t('contactEmail')) ?></li>
+        </ul>
+    </div>
+</div>
+<div class="one-third last">
+    <div class="infobox">
+        <h5 class="section-title"><?= HApp::t('about_2_1', 'texts') ?></h5>
+        <ul class="arrow dotted marginTop">
+            <?php foreach (Yii::app()->params['team'] as $member): ?>
+                <li><?= $member ?>.</li>
+            <?php endforeach; ?>
+        </ul>
+        <h5 class="section-title marginTop"><?= HApp::t('about_2_2', 'texts') ?></h5>
+        <div class="marginTop"><?= HApp::t('about_2_2.1', 'texts') ?></div>
+        <div><?= HApp::t('about_2_2.2', 'texts') ?></div>
+        <ul class="arrow-2 dotted marginTop">
+            <li><strong><?= HApp::t('email') ?>:</strong> <?= CHtml::link(HApp::t('jobsEmail'), 'mailto:'.HApp::t('jobsEmail')) ?></li>
+        </ul>
+    </div>
+</div>
