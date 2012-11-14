86fd849101cd68ec1b29e9180b36d34ca0825476
FabianoLothor <fabiano.lothor@gmail.com>
2012-11-14 01:55:26 -0300

    Adição/Modificação nos textos para a página de Sobre/Termos do site.

--- project/protected/messages/pt_br/texts.php
+++ project/protected/messages/pt_br/texts.php
@@ -2,8 +2,11 @@
 
 return array(
     // About
-    'about_1_1.1' => 'O <strong>YouTroll</strong>, é uma rede de compartilhamento de conteúdo cômico.',
-    'about_1_1.2' => 'A proposta é oferecer um local onde os usuários possam visualizar imagens de diversos canais, em um só local, além de possuirem liberdade de publicar suas próprias criações de forma fácil e rápida.',
+    'about_1_0.1' => 'O <strong>YouTroll</strong>, é uma rede de compartilhamento de conteúdo cômico.',
+    'about_1_0.2' => 'A proposta é oferecer um local onde os usuários possam visualizar imagens de diversos canais, em um só local, além de possuirem liberdade de publicar suas próprias criações de forma fácil e rápida.',
+
+    'about_1_1' => 'Produza seu próprio conteúdo',
+    'about_1_1.1' => 'Você pode produzir e divulgar para seus fãs e amigos, imagens que satirizam o seu dia-a-dia, ou mesmo, cenas cotidianas. As publicações podem ser tanto enviadas, quanto criadas no próprio site.',
     
     'about_1_2' => 'Acompanhe seus canais favoritos',
     'about_1_2.1' => 'Agora ficou fácil ver aquela imagem engraçada que o seu canal favorito acabou de publicar.',
@@ -12,11 +15,8 @@ return array(
     'about_1_3' => 'Compartilhe com seus amigos',
     'about_1_3.1' => 'No <strong>YouTroll</strong> podes compartilhar as imagens com todos os seus amigos, através das principais redes sociais de forma rápida e objetiva.',
     
-    'about_1_4' => 'Produza seu próprio conteúdo',
-    'about_1_4.1' => 'Você pode produzir e divulgar para seus fãs e amigos, imagens que satirizam o seu dia-a-dia, ou mesmo, cenas cotidianas. As publicações podem ser tanto enviadas, quanto criadas no próprio site.',
-    
-    'about_1_5' => 'Contato',
-    'about_1_5.1' => 'Sinta-se à vontade para entrar em contato conosco em caso de dúvidas sobre nossos serviços, propostas ou parcerias, estaremos sempre à disposição.',
+    'about_1_4' => 'Contato',
+    'about_1_4.1' => 'Sinta-se à vontade para entrar em contato conosco em caso de dúvidas sobre nossos serviços, propostas ou parcerias, estaremos sempre à disposição.',
     
     'about_2_1' => 'Equipe',
     
@@ -25,35 +25,31 @@ return array(
     'about_2_2.2' => 'Envie-nos seu CV e faça parte de nossa equipe.',
     
     // Terms
-    'terms_revision' => '<strong>Data da Última Revisão:</strong> 14/11/2012',
+    'terms_revision' => '<strong>Data da Última Revisão:</strong> 14/11/2012 - 1ª Versão.',
+    
     'terms_1_1.1' => 'O idioma da versão original deste acordo é o português (Brasil).',
     'terms_1_1.2' => 'Em caso de conflito de qualquer versão traduzida deste acordo com a versão em português, a versão em português prevalecerá.',
     
-    'terms_1_2' => 'Declaração de Direitos e Responsabilidades',
-    'terms_1_2.1' => '',
+    'terms_1_2' => 'Aceitação',
+    'terms_1_2.1' => 'Estes Termos e Condições de Utilização dos Serviços ("Termos"), regulam o seu acesso e utilização dos serviços, assim como qualquer informação, textos, gráficos, fotos ou quaisquer outros materiais baixados ou disponibilizados. O seu acesso e utilização dos serviços, encontram-se sujeitos à aceitação e cumprimento destes Termos. Ao acessar ou utilizar os serviços, você aceita que se encontra vinculado aos Termos.',
+    'terms_1_2.2' => 'Quanto à capacidade para aceitar os Termos, Você afirma ser maior de 18 anos, ser menor emancipado, ou estar de posse de autorização legal dos pais ou de tutores e plenamente capaz de consentir com os termos, condições, obrigações, afirmações, representações e garantias descritas nestes Termos, afim de obedecê-los e cumpri-los. Em qualquer circunstância, Você afirma ter mais de 18 anos, visto que o site do YouTroll não é projetado para jovens menores de 18 anos. Se você tiver menos de 18 anos, não deverá utilizar o YouTroll. Você deverá conversar com seus pais sobre quais sites são apropriados para si.',
     
-    'terms_1_3' => 'Aceitação',
-    'terms_1_3.1' => '',
-    
-    'terms_1_4' => 'Privacidade',
+    'terms_1_3' => 'Privacidade',
+    'terms_1_3.1' => 'Sua privacidade é muito importante para nós, por isso, nós projetamos nossa {privacy}, de modo que qualquer informação que você disponibilizar em sua conta, fiquem amparadas pela mesma, a qual regula as nossas operações de coleta e utilização da sua informação. Você concorda que, ao utilizar os nossos serviços, estará consentindo com a coleta e utilização (tal como definido na Política de Privacidade) desta informação, incluindo a transferência da mesma para efeitos de armazenagem, tratamento e utilização pelo YouTroll. Como parte integrante da prestação e disponibilização dos serviços, o YouTroll poderá efetuar certas comunicações, incluindo anúncios de serviço e mensagens administrativas. Tais comunicações são consideradas parte dos serviços e da sua conta no YouTroll, não lhe sendo permitido recusá-las.',
+
+    'terms_1_4' => 'Compartilhando Conteúdo e Informação',
     'terms_1_4.1' => '',
     
-    'terms_1_5' => 'Compartilhando Conteúdo e Informação',
+    'terms_1_5' => 'Segurança',
     'terms_1_5.1' => '',
     
-    'terms_1_6' => 'Segurança',
+    'terms_1_6' => 'Proteção dos Direitos de Outras Pessoas',
     'terms_1_6.1' => '',
     
-    'terms_1_7' => 'Proteção dos Direitos de Outras Pessoas',
+    'terms_1_7' => 'Alterações',
     'terms_1_7.1' => '',
     
-    'terms_1_8' => 'Alterações',
+    'terms_1_8' => 'Conclusão',
     'terms_1_8.1' => '',
-    
-    'terms_1_9' => 'Conclusão',
-    'terms_1_9.1' => '',
-    
-    'terms_1_' => '',
-    'terms_1_.1' => '',
     
 );
