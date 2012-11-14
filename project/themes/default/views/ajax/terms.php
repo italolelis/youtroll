3156d31131ede65c75214f172f5dfcacf83a5d1f
FabianoLothor <fabiano.lothor@gmail.com>
2012-11-14 04:02:02 -0300

    Finalização dos Termos e Condições de Uso do Serviço.

--- project/themes/default/views/ajax/terms.php
+++ project/themes/default/views/ajax/terms.php
@@ -22,18 +22,33 @@
     
     <h5 class="section-title marginTop"><?= HApp::t('terms_1_4', 'texts') ?></h5>
     <div class="marginTop"><?= HApp::t('terms_1_4.1', 'texts') ?></div>
+    <div class="marginTop"><?= HApp::t('terms_1_4.2', 'texts') ?></div>
+    <div class="marginTop"><?= HApp::t('terms_1_4.3', 'texts') ?></div>
+    <div class="marginTop"><?= HApp::t('terms_1_4.4', 'texts') ?></div>
+    <div class="marginTop"><?= HApp::t('terms_1_4.5', 'texts') ?></div>
     
     <h5 class="section-title marginTop"><?= HApp::t('terms_1_5', 'texts') ?></h5>
     <div class="marginTop"><?= HApp::t('terms_1_5.1', 'texts') ?></div>
+    <div class="marginTop"><?= HApp::t('terms_1_5.2', 'texts') ?></div>
+    <div class="marginTop"><?= HApp::t('terms_1_5.3', 'texts') ?></div>
     
     <h5 class="section-title marginTop"><?= HApp::t('terms_1_6', 'texts') ?></h5>
     <div class="marginTop"><?= HApp::t('terms_1_6.1', 'texts') ?></div>
+    <div class="marginTop"><?= HApp::t('terms_1_6.2', 'texts') ?></div>
+    <div class="marginTop"><?= HApp::t('terms_1_6.3', 'texts') ?></div>
+    <ul class="arrow dotted marginTop">
+        <li><?= HApp::t('terms_1_6.4', 'texts') ?></li>
+        <li><?= HApp::t('terms_1_6.5', 'texts') ?></li>
+        <li><?= HApp::t('terms_1_6.6', 'texts') ?></li>
+    </ul>
     
     <h5 class="section-title marginTop"><?= HApp::t('terms_1_7', 'texts') ?></h5>
     <div class="marginTop"><?= HApp::t('terms_1_7.1', 'texts') ?></div>
     
     <h5 class="section-title marginTop"><?= HApp::t('terms_1_8', 'texts') ?></h5>
     <div class="marginTop"><?= HApp::t('terms_1_8.1', 'texts') ?></div>
+    <div class="marginTop"><?= HApp::t('terms_1_8.2', 'texts') ?></div>
+    <div class="marginTop"><?= HApp::t('terms_1_8.3', 'texts') ?></div>
     
     <ul class="arrow-2 dotted marginTop">
         <li><?= HApp::t('terms_revision', 'texts') ?></li>
