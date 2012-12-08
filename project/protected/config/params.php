3d37a89f6f6ef422453f24c3d009641643141560
FabianoLothor <fabiano.lothor@gmail.com>
2012-12-08 20:23:06 -0300

    Adição de novos parametros padrões.

--- project/protected/config/params.php
+++ project/protected/config/params.php
@@ -4,6 +4,10 @@ return array(
     'adminEmail' => 'admin@youtroll.com.br',
     'persistenceServer'=> 'http://localhost/youtroll/rest/',
     
+    'fb_id' => '421726281227767',
+    'fb_secret' => 'a1601906b0b78180ebc7654bf0ad332a',
+    'disqus_shortname' => 'ytroll',
+    
     'allowedExtensions' => array('jpg', 'gif', 'png'),
     'maxSizeUpload' => 1048576, // 1MB
     'maxPublications' => 12,
