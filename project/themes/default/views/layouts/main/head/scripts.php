109ec90b89a7c0f8157c095b3888e4bcd1232f29
FabianoLothor <fabiano.lothor@gmail.com>
2012-12-15 15:34:40 -0300

    Importação do jquery.

--- project/themes/default/views/layouts/main/head/scripts.php
+++ project/themes/default/views/layouts/main/head/scripts.php
@@ -1,3 +1,5 @@
+<?php Yii::app()->clientScript->registerCoreScript('jquery'); ?>
+
 <?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/themeModernizrCustom.js') ?>
 <?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/themeCustom.js') ?>
 <?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/themeJCycle.js') ?>
