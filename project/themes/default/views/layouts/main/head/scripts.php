91ddcdfc2f8132774df0ce9189adee23e27fd8c7
FabianoLothor <fabiano.lothor@gmail.com>
2012-12-15 23:10:43 -0300

    Adição do código do Google Analytics.

--- project/themes/default/views/layouts/main/head/scripts.php
+++ project/themes/default/views/layouts/main/head/scripts.php
@@ -16,3 +16,18 @@
 
 <?= CHtml::scriptFile(Yii::app()->baseUrl . '/resources/js/general.js') ?>
 <?= CHtml::scriptFile(Yii::app()->theme->baseUrl . '/resources/js/script.js') ?>
+
+<script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-37069336-1']);
+    _gaq.push(['_setDomainName', 'youtroll.com.br']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+</script>
